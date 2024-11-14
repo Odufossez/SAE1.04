7ea5aa52451ce48cbf052b413dc07d0527ec59f3
ODufossez <oscardufossez@gmail.com>
2024-11-14 14:07:36 +0100

    Rectif des tables

--- sql_projet.sql
+++ sql_projet.sql
@@ -18,113 +18,118 @@ drop table if exists Adhérent;
 CREATE TABLE Adhérent(
    Id_Adhérent INT AUTO_INCREMENT NOT NULL,
    Nom VARCHAR(50),
-   Prénom VARCHAR(50),
+   Prenom VARCHAR(50),
    Adresse VARCHAR(50),
    Téléphone VARCHAR(10),
    PRIMARY KEY(Id_Adhérent)
 );
 
-CREATE TABLE Parcelle(
-   Id_Parcelle INT AUTO_INCREMENT NOT NULL,
-   Longueur DECIMAL(4,2),
-   Largeur DECIMAL(4,2),
-   PRIMARY KEY(Id_Parcelle)
+CREATE TABLE Fruits_Légumes_et_aromate(
+    Id_FruitLégume INT AUTO_INCREMENT NOT NULL,
+    Libellé VARCHAR(20),
+
+    PRIMARY KEY(Id_FruitLégume)
 );
 
-CREATE TABLE Fruits_Légumes_et_aromate(
-   Id_FruitLégume INT AUTO_INCREMENT NOT NULL,
-   Libellé VARCHAR(20),
-   PRIMARY KEY(Id_FruitLégume)
+CREATE TABLE Parcelle(
+    Id_Parcelle INT AUTO_INCREMENT NOT NULL,
+    Longueur DECIMAL(4,2),
+    Largeur DECIMAL(4,2),
+    Plante_id INT,
+
+    PRIMARY KEY(Id_Parcelle),
+    FOREIGN KEY(plante_id) REFERENCES Fruits_Légumes_et_aromate(Id_FruitLégume)
 );
 
 CREATE TABLE Dates(
-   JJ_MM_AAAA DATE,
-   PRIMARY KEY(JJ_MM_AAAA)
+    JJ_MM_AAAA DATE,
+    PRIMARY KEY(JJ_MM_AAAA)
 );
 
 CREATE TABLE Actions(
-   Id_Actions INT AUTO_INCREMENT NOT NULL,
-   Libellé VARCHAR(20),
-   PRIMARY KEY(Id_Actions)
+    Id_Actions INT AUTO_INCREMENT NOT NULL,
+    Libellé VARCHAR(20),
+
+    PRIMARY KEY(Id_Actions)
 );
 
 CREATE TABLE Défaut(
-   Id_Défaut INT AUTO_INCREMENT NOT NULL,
-   Libellé VARCHAR(20),
-   Id_Actions INT,
-   PRIMARY KEY(Id_Défaut),
-   FOREIGN KEY(Id_Actions) REFERENCES Actions(Id_Actions)
+    Id_Défaut INT AUTO_INCREMENT NOT NULL,
+    Libellé VARCHAR(20),
+    Id_Actions INT,
+
+    PRIMARY KEY(Id_Défaut),
+    FOREIGN KEY(Id_Actions) REFERENCES Actions(Id_Actions)
 );
 
 CREATE TABLE Compost(
-   Id_Compost INT AUTO_INCREMENT NOT NULL ,
-   Taille VARCHAR(20),
-   Localisation VARCHAR(20),
-   PRIMARY KEY(Id_Compost)
+    Id_Compost INT AUTO_INCREMENT NOT NULL ,
+    Taille VARCHAR(20),
+    Localisation VARCHAR(20),
+
+    PRIMARY KEY(Id_Compost)
 );
 
 CREATE TABLE Mois(
-   Id_Mois INT AUTO_INCREMENT NOT NULL ,
-   Libellé VARCHAR(20),
-   PRIMARY KEY(Id_Mois)
+    Id_Mois INT AUTO_INCREMENT NOT NULL ,
+    Libellé VARCHAR(20),
+
+    PRIMARY KEY(Id_Mois)
 );
 
 CREATE TABLE Traitement(
-   Id_traitement INT AUTO_INCREMENT NOT NULL ,
-   Libellé VARCHAR(20),
-   Id_Actions INT,
-   PRIMARY KEY(Id_traitement),
-   FOREIGN KEY(Id_Actions) REFERENCES Actions(Id_Actions)
+    Id_traitement INT AUTO_INCREMENT NOT NULL ,
+    Libellé VARCHAR(20),
+    Id_Actions INT,
+
+    PRIMARY KEY(Id_traitement),
+    FOREIGN KEY(Id_Actions) REFERENCES Actions(Id_Actions)
 );
 
 CREATE TABLE Pousse(
-   Id_FruitLégume INT AUTO_INCREMENT NOT NULL,
-   Id_Mois INT,
-   PRIMARY KEY(Id_FruitLégume, Id_Mois),
-   FOREIGN KEY(Id_FruitLégume) REFERENCES Fruits_Légumes_et_aromate(Id_FruitLégume),
-   FOREIGN KEY(Id_Mois) REFERENCES Mois(Id_Mois)
+    Id_FruitLégume INT AUTO_INCREMENT NOT NULL,
+    Id_Mois INT,
+
+    PRIMARY KEY(Id_FruitLégume, Id_Mois),
+    FOREIGN KEY(Id_FruitLégume) REFERENCES Fruits_Légumes_et_aromate(Id_FruitLégume),
+    FOREIGN KEY(Id_Mois) REFERENCES Mois(Id_Mois)
 );
 
 CREATE TABLE Attendre(
-   Id_traitement INT ,
-   Id_traitement_1 INT,
-   PRIMARY KEY(Id_traitement, Id_traitement_1),
-   FOREIGN KEY(Id_traitement) REFERENCES Traitement(Id_traitement),
-   FOREIGN KEY(Id_traitement_1) REFERENCES Traitement(Id_traitement)
+    Id_traitement INT ,
+    Id_traitement_1 INT,
+
+    PRIMARY KEY(Id_traitement, Id_traitement_1),
+    FOREIGN KEY(Id_traitement) REFERENCES Traitement(Id_traitement),
+    FOREIGN KEY(Id_traitement_1) REFERENCES Traitement(Id_traitement)
 );
 
 CREATE TABLE Entretient(
-   Id_Adhérent INT,
-   JJ_MM_AAAA DATE,
-   Id_Actions INT,
-   Id_Compost INT,
-   Quantité DECIMAL(4,2),
-   PRIMARY KEY(Id_Adhérent, JJ_MM_AAAA, Id_Actions, Id_Compost),
-   FOREIGN KEY(Id_Adhérent) REFERENCES Adhérent(Id_Adhérent),
-   FOREIGN KEY(JJ_MM_AAAA) REFERENCES Dates(JJ_MM_AAAA),
-   FOREIGN KEY(Id_Actions) REFERENCES Actions(Id_Actions),
-   FOREIGN KEY(Id_Compost) REFERENCES Compost(Id_Compost)
-);
+    Id_Adhérent INT,
+    JJ_MM_AAAA DATE,
+    Id_Actions INT,
+    Id_Compost INT,
+    Quantité DECIMAL(4,2),
 
-CREATE TABLE Pousse_dans(
-   Id_Parcelle INT,
-   Id_FruitLégume INT,
-   PRIMARY KEY(Id_Parcelle, Id_FruitLégume),
-   FOREIGN KEY(Id_Parcelle) REFERENCES Parcelle(Id_Parcelle),
-   FOREIGN KEY(Id_FruitLégume) REFERENCES Fruits_Légumes_et_aromate(Id_FruitLégume)
+    PRIMARY KEY(Id_Adhérent, JJ_MM_AAAA, Id_Actions, Id_Compost),
+    FOREIGN KEY(Id_Adhérent) REFERENCES Adhérent(Id_Adhérent),
+    FOREIGN KEY(JJ_MM_AAAA) REFERENCES Dates(JJ_MM_AAAA),
+    FOREIGN KEY(Id_Actions) REFERENCES Actions(Id_Actions),
+    FOREIGN KEY(Id_Compost) REFERENCES Compost(Id_Compost)
 );
 
 CREATE TABLE Fait(
-   Id_Adhérent INT,
-   Id_Parcelle INT,
-   JJ_MM_AAAA DATE,
-   Id_Actions INT,
-   Quantité DECIMAL(4,2),
-   PRIMARY KEY(Id_Adhérent, Id_Parcelle, JJ_MM_AAAA, Id_Actions),
-   FOREIGN KEY(Id_Adhérent) REFERENCES Adhérent(Id_Adhérent),
-   FOREIGN KEY(Id_Parcelle) REFERENCES Parcelle(Id_Parcelle),
-   FOREIGN KEY(JJ_MM_AAAA) REFERENCES Dates(JJ_MM_AAAA),
-   FOREIGN KEY(Id_Actions) REFERENCES Actions(Id_Actions)
+    Id_Adhérent INT,
+    Id_Parcelle INT,
+    JJ_MM_AAAA DATE,
+    Id_Actions INT,
+    Quantité DECIMAL(4,2),
+
+    PRIMARY KEY(Id_Adhérent, Id_Parcelle, JJ_MM_AAAA, Id_Actions),
+    FOREIGN KEY(Id_Adhérent) REFERENCES Adhérent(Id_Adhérent),
+    FOREIGN KEY(Id_Parcelle) REFERENCES Parcelle(Id_Parcelle),
+    FOREIGN KEY(JJ_MM_AAAA) REFERENCES Dates(JJ_MM_AAAA),
+    FOREIGN KEY(Id_Actions) REFERENCES Actions(Id_Actions)
 );
 
 -- AJOUT DES DONNÉES DANS LES TABLES
